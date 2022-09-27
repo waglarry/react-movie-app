0ff5e1c6f9264402fd6903052e83f4baddc02214
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-09-27 16:03:14 +0000

    Created new files and added codes

--- src/Components/NavItems/NavLinks/NavLinks.jsx
+++ src/Components/NavItems/NavLinks/NavLinks.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { NavLink } from "react-router-dom";
 import { FaTimes } from 'react-icons/fa';
-import { FaSearch } from 'react-icons/fa';
+// import { FaSearch } from 'react-icons/fa';
+import './NavLinks.css'
 
 const NavLinks = () => {
+    window.addEventListener('scroll',function(){
+        const navbar = document.getElementById("header");
+        navbar.classList.toggle("sticky", window.scrollY > 0);
+    });
+
+    const NavLinkStyle = ({ isActive }) =>{
+      return {
+        color: isActive ? 'dodgerblue' : '#fff'
+      }
+    }
+
+    const navRef = useRef()
+
+    const showNavbar = () => {
+      navRef.current.classList.toggle("reponsiveNav")
+    }
+
   return (
     <div>
         <div className="navLinks">
-              <NavLink to="/movies" className='navLink'>
+              <NavLink to="/movies" className='navLink' style={NavLinkStyle} onClick={showNavbar}>
                 Movies
               </NavLink>
-              <NavLink to="/tvshows" className='navLink'>
+              <NavLink to="/tvshows" className='navLink' style={NavLinkStyle} onClick={showNavbar}>
                 Tv Shows
               </NavLink>
               <button className='navBtn navCloseBtn'>
