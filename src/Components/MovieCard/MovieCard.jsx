e5decfc0692a77b53944adc7b5f07c4056aeb3ad
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-09-30 17:30:55 +0000

    design movieCard

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -5,9 +5,12 @@ import { API_IMG } from '../API/URL'
 const MovieCard = ({title, released, overview, poster, rate, wallpaper}) => {
   return (
     <div className='movieCard'>
-      <img className='poster' src={API_IMG+poster} alt="" />
+      <div className='posterBox'>
+        <img className='poster' src={API_IMG+poster} alt="" />
+        <span className='rate'>Rating {rate.toFixed(1)}</span>
+      </div>
       <div className='details'>
-        <h4 className='title'>Title: {title}</h4>
+        <h4 className='title'>{title}</h4>
         <span className='date'>Released: {released}</span>
       </div>
     </div>
