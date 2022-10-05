0e33452fd6c438a780d7172478d669bc69efaf94
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-05 16:06:21 +0000

    add navPage and fixed filter links

--- src/Components/API/GetMovieContent.jsx
+++ src/Components/API/GetMovieContent.jsx
@@ -1,39 +1,178 @@
-import React from 'react';
+import React, { useState } from "react";
 import Axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import MovieCard from "../Cards/MovieCard/MovieCard";
-import { API_KEY } from './URL';
+import { API_KEY } from "./URL";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
-const GetMovieContent = ({content, filter}) => {
-    const { data, isLoading, isError } = useQuery(
-        ["content"],
-        () => {
-            return Axios.get(`https://api.themoviedb.org/3/${content}/${filter}?api_key=${API_KEY}&language=en-US&page=1`).then((response) => response.data.results)
-        }
-      );
+const GetMovieContent = ({ content, contentTitle, filter }) => {
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  const [isDisabled, setDisabled] = useState(false);
+  const [movieFilter, setMovieFilter] = useState("popular");
+  const [movies, setMovies] = useState({});
+  const [searchKeyword, setSearchKeyword] = useState("")
 
-    if(isLoading) return <h1>Loading...</h1>
-    if(isError) return <h1>Error...</h1>
+  const { isLoading, isError, isFetching } = useQuery(
+    ["content", currentPageNumber, movieFilter],
+    () => {
+      return Axios.get(
+        `https://api.themoviedb.org/3/${content}/${movieFilter}?api_key=${API_KEY}&language=en-US&page=${currentPageNumber}`,
+        { keepPreviousData: true }
+      ).then((response) => response.data);
+    },
+    { onSuccess: setMovies }
+  );
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>Error...</h1>;
+
+  const setMovieType = (movieType) => {
+    if (movieType === "Now Playing") {
+      setMovieFilter("now_playing");
+    } else if (movieType === "Upcoming") {
+      setMovieFilter("upcoming");
+    } else if (movieType === "Top Rated") {
+      setMovieFilter("top_rated");
+    } else if (movieType === "Airing Today") {
+      setMovieFilter("airing_today");
+    } else if (movieType === "On Tv") {
+      setMovieFilter("on_the_air");
+    } else {
+      setMovieFilter(movieType.toLowerCase());
+    }
+  };
+
+  const nextPage = () => {
+    setCurrentPageNumber(currentPageNumber + 1);
+    // window.scroll(0,0)
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const previousPage = () => {
+    setCurrentPageNumber(currentPageNumber - 1);
+    // window.scroll(0,0)
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const searchMovie = (event) =>{
+    if(event.key==="Enter"){
+    const { isLoading, isError } = useQuery(
+      ["content"],
+      () => {
+        return Axios.get(
+          `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${searchKeyword}`,
+          { keepPreviousData: true }
+        ).then((response) => response.data);
+      },
+      { onSuccess: setMovies }
+    );
+  
+    if (isLoading) return <h1>Loading...</h1>;
+    if (isError) return <h1>Error...</h1>;
+    }
+  }
+
+  // console.log(searchKeyword);
 
   return (
     <div>
-    <div className="mainContentBox">
-      <div className="contentBox">
-        {data.map((e) => (
-          <MovieCard
-            key={e.id}
-            title={e.title || e.name}
-            released={e.first_air_date || e.release_date}
-            poster={e.poster_path}
-            rate={e.vote_average}
-            overview={e.overview}
-            wallpaper={e.backdrop_path}
-        />
-        ))}
+      <div
+        className="TopPageBG"
+        style={{
+          backgroundColor: "#ccc",
+        }}
+      >
+        <div className="NavTopPage">
+          <h1 className="contentTitle">{contentTitle}</h1>
+          <div className="contentDetails">
+            <ul className="filterControls">
+              {filter.map((filt, id) => {
+                return (
+                  <li className="filter" key={id}>
+                    <Link
+                      to=""
+                      className="filterLink"
+                      name={filt}
+                      onClick={(e) => {
+                        setMovieType(e.target.name);
+                      }}
+                    >
+                      {filt}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="navSearchBox">
+              <input className="textBox" type="text" placeholder="Search" onChange={(e) => {setSearchKeyword(e.target.value)}} value={searchKeyword} onKeyPress={searchMovie}/>
+              <button className="searchBtn">
+                <FaSearch />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+      <div className="ControlsTopDiv">
+        <button
+          type="button"
+          className="topButton"
+          disabled={currentPageNumber === 1 ? !isDisabled : isDisabled}
+          onClick={previousPage}
+        >
+          Previous
+        </button>
+        <button
+          type="button"
+          className="topButton"
+          disabled={
+            currentPageNumber !== movies.total_pages ? isDisabled : !isDisabled
+          }
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div>
+      <div className="mainContentBox">
+        <div className="contentBox">
+          {movies.results &&
+            movies.results.map((e) => (
+              <MovieCard
+                key={e.id}
+                title={e.title || e.name}
+                released={e.first_air_date || e.release_date}
+                poster={e.poster_path}
+                rate={e.vote_average}
+                overview={e.overview}
+                wallpaper={e.backdrop_path}
+              />
+            ))}
+        </div>
+      </div>
+      <div className="paginationControl">
+        <button
+          type="button"
+          className="downButton"
+          disabled={currentPageNumber === 1 ? !isDisabled : isDisabled}
+          onClick={previousPage}
+        >
+          Previous
+        </button>
+        <span className="CurrentPageNumber">{currentPageNumber}</span>
+        <button
+          type="button"
+          className="downButton"
+          disabled={
+            currentPageNumber !== movies.total_pages ? isDisabled : !isDisabled
+          }
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div>
+      {isFetching ? <span> Loading...</span> : null}{" "}
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default GetMovieContent
+export default GetMovieContent;
--- src/Components/API/GetPeopleContent.jsx
+++ src/Components/API/GetPeopleContent.jsx
@@ -1,24 +1,88 @@
-import React from 'react'
+import React, { useState } from "react";
 import Axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import PeopleCard from '../Cards/PeopleCard/PeopleCard';
 import { API_KEY } from './URL';
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
-const GetPeopleContent = ({content, filter}) => {
-    const { data, isLoading, isError } = useQuery(
-        ["content"],
-        () => {
-            return Axios.get(`https://api.themoviedb.org/3/${content}/${filter}?api_key=${API_KEY}&language=en-US&page=1`).then((response) => response.data.results)
-        }
-      );
+const GetPeopleContent = ({content,contentTitle, filter}) => {
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  const [isDisabled, setDisabled] = useState(false);
+  const [movieFilter, setMovieFilter] = useState("popular");
+  const [people, setPeople] = useState({});
+
+  const { isLoading, isError, isFetching } = useQuery(
+    ["content", currentPageNumber, movieFilter],
+    () => {
+      return Axios.get(
+        `https://api.themoviedb.org/3/${content}/${movieFilter}?api_key=${API_KEY}&language=en-US&page=${currentPageNumber}`,
+        { keepPreviousData: true }
+      ).then((response) => response.data);
+    },
+    { onSuccess: setPeople }
+  );
 
       if(isLoading) return <h1>Loading...</h1>
       if(isError) return <h1>Error...</h1>
+
+      const setMovieType = (movieType) => {
+          setMovieFilter(movieType.toLowerCase());
+      };
+
+      const nextPage = () => {
+        setCurrentPageNumber(currentPageNumber + 1);
+        // window.scroll(0,0)
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+      };
+    
+      const previousPage = () => {
+        setCurrentPageNumber(currentPageNumber - 1);
+        // window.scroll(0,0)
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+      };
+      
   return (
     <div>
+        <div
+        className="TopPageBG"
+        style={{
+          backgroundColor: "#ccc",
+        }}
+      >
+        <div className="NavTopPage">
+          <h1 className="contentTitle">{contentTitle}</h1>
+          <div className="contentDetails">
+            <ul className="filterControls">
+              {filter.map((filt, id) => {
+                return (
+                  <li className="filter" key={id}>
+                    <Link
+                      to=""
+                      className="filterLink"
+                      name={filt}
+                      onClick={(e) => {
+                        setMovieType(e.target.name);
+                      }}
+                    >
+                      {filt}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="navSearchBox">
+              <input className="textBox" type="text" placeholder="Search" />
+              <button className="searchBtn">
+                <FaSearch />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     <div className="mainContentBox">
       <div className="contentBox">
-        {data.map((e) => (
+        {people.results && people.results.map((e) => (
           <PeopleCard
             key={e.id}
             poster={e.profile_path}
@@ -28,6 +92,28 @@ const GetPeopleContent = ({content, filter}) => {
         ))}
       </div>
     </div>
+    <div className="paginationControl">
+        <button
+          type="button"
+          className="downButton"
+          disabled={currentPageNumber === 1 ? !isDisabled : isDisabled}
+          onClick={previousPage}
+        >
+          Previous
+        </button>
+        <span className="CurrentPageNumber">{currentPageNumber}</span>
+        <button
+          type="button"
+          className="downButton"
+          disabled={
+            currentPageNumber !== people.total_pages ? isDisabled : !isDisabled
+          }
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div>
+      {isFetching ? <span> Loading...</span> : null}{" "}
   </div>
   )
 }
