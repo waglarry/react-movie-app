5abef876588b439ad4f226d61458ffea317dc5ef
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-05 16:07:12 +0000

    add new objects to QueryClient

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ import People from './Components/Pages/People/people';
 import Error from './Components/Pages/Error/error';
 
 function App() {
-  const client = new QueryClient();
+  const client = new QueryClient({
+    defaultOptions: {
+    queries: {
+    refetchOnWindowFocus: false,
+    },
+    },
+    });
   return (
       <div className="App">
         <QueryClientProvider client={client}>
