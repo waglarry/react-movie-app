447be6bbea29fa3589f958517af6c277964fe89c
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-07 09:46:26 +0000

    style footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,10 @@ const Footer = () => {
             <div className="footerContainer">
                 <div className="containerBox">
                 <div className="footerLogo">
-                    <h1><span>🏚</span> Waga's<span>HUB</span></h1>
+                    <h1 className='footerLogoH1'><span>🏚</span> Waga's<span>HUB</span></h1>
+                    <p className="logoContent">
+                        Find all super exciting movies here on the Hub...
+                    </p>
                 </div>
                 <div className="pages">
                     <h3 className="footerTitle">Pages</h3>
@@ -22,18 +25,18 @@ const Footer = () => {
                 <div className="howTo">
                     <h3 className="footerTitle">How To...</h3>
                     <ul className="Links">
-                        <li>Watch</li>
-                        <li>Download</li>
-                        <li>View</li>
+                        <li className='footerLinks'>Watch</li>
+                        <li className='footerLinks'>Download</li>
+                        <li className='footerLinks'>View</li>
                     </ul>
                 </div>
                 <div className="contact">
                     <h3 className="footerTitle">Contact Us</h3>
                     <ul className="Links">
-                        <li>Facebook</li>
-                        <li>Instagram</li>
-                        <li>Twitter</li>
-                        <li>Telegram</li>
+                        <li className='footerLinks'>Facebook</li>
+                        <li className='footerLinks'>Instagram</li>
+                        <li className='footerLinks'>Twitter</li>
+                        <li className='footerLinks'>Telegram</li>
                     </ul>
                 </div>
                 </div>
