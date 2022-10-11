98ba225327b0ce3b906b42bd236060716b7c390a
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-11 17:26:59 +0000

    change movie content function

--- src/Components/Cards/OverviewCard/OverviewCard.jsx
+++ src/Components/Cards/OverviewCard/OverviewCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { API_IMG, unavailable } from '../../API/URL'
 
 const OverviewCard = ({movie, selectCard}) => {
-    const setMovieContent = () => {
+    const setOverviewContent = () => {
         selectCard(movie)
         window.scroll(0, 0)
     }
   return (
     <div className='movieCard'>
-    <div className='posterBox' onClick={setMovieContent}>
+    <div className='posterBox' onClick={setOverviewContent}>
       <img className='poster' src={ movie.poster_path ? API_IMG+movie.poster_path : unavailable} alt={movie.title} />
       <span className='rate'>Rated - {movie.vote_average.toFixed(1)}</span>
     </div>
