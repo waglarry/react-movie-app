8342680dacfbb75522b4672fd58e569c3855d9de
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-14 20:56:35 +0000

    remove p tah from btn in movie card

--- src/Components/Cards/MovieCard/MovieCard.jsx
+++ src/Components/Cards/MovieCard/MovieCard.jsx
@@ -3,12 +3,12 @@ import './MovieCard.css'
 import { API_IMG, unavailable } from '../../API/URL'
 import { Link } from 'react-router-dom'
 
-const MovieCard = ({title, released, overview, poster, rate}) => {
+const MovieCard = ({title, released, poster, rate}) => {
   return (
     <div className='movieCard'>
       <div className='posterBox'>
         <img className='poster' src={ poster ? API_IMG+poster : unavailable} alt={title} />
-        <div className='overview'>Overview <p>{overview}</p> <span><Link to={`/overview`}><button className='posterViewBtn'>View</button></Link></span></div>
+        <div className='overview'><Link to={`/overview`}><button className='posterViewBtn'>View</button></Link></div>
         <span className='rate'>Rated - {rate.toFixed(1)}</span>
       </div>
       <div className='details'>
