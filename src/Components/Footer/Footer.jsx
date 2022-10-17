7b92e7f62dab0ec2e8986cc5c8abac04c081ed2b
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-17 08:36:33 +0000

    add links to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -20,22 +20,22 @@ const Footer = () => {
                         <li><Link className='footerLinks' to='/movies'>Movies</Link></li>
                         <li><Link className='footerLinks' to='/tvshows'>Tv Shows</Link></li>
                         <li><Link className='footerLinks' to='/people'>People</Link></li>
+                        <li><Link className='footerLinks' to='/overview'>Overview</Link></li>
                     </ul>
                 </div>
                 <div className="howTo">
                     <h3 className="footerTitle">How To...</h3>
                     <ul className="Links">
-                        <li className='footerLinks'>Watch</li>
-                        <li className='footerLinks'>Download</li>
-                        <li className='footerLinks'>View</li>
+                        <li><Link className='footerLinks' to='/howToDownload'>Download</Link></li>
+                        <li><Link className='footerLinks' to='/howToWatchTrailer'>Watch Trailer</Link></li>
                     </ul>
                 </div>
                 <div className="contact">
                     <h3 className="footerTitle">Contact Us</h3>
                     <ul className="Links">
+                        <li><a className='footerLinks' target="_blank" href="https://t.me/joinchat/uqaW_ETwe1g1MDZk" rel="noopener noreferrer">Telegram</a></li>
                         <li><a className='footerLinks' target="_blank" href="https://instagram.com/iamkofi.emma?igshid=YmMyMTA2M2Y=" rel="noopener noreferrer">Instagram</a></li>
                         <li><a className='footerLinks' target="_blank" href="https://twitter.com/kofi_waga?s=21" rel="noopener noreferrer">Twitter</a></li>
-                        <li><a className='footerLinks' target="_blank" href="https://t.me/joinchat/uqaW_ETwe1g1MDZk" rel="noopener noreferrer">Telegram</a></li>
                     </ul>
                 </div>
                 </div>
