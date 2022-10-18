f7a187a8292fd7ddc95e6d6921e2dd3032a3160a
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-18 15:48:18 +0000

    create overviewPeopleCard

---
+++ src/Components/Cards/OverviewPeopleCard/OverviewPeopleCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { API_IMG, unavailable } from '../../API/URL'
+
+const OverviewPeopleCard = ({movie, selectCard}) => {
+    const setOverviewContent = () => {
+        selectCard(movie)
+        window.scroll(0, 0)
+    }
+  return (
+    <div className='movieCard'>
+    <div className='posterBox' onClick={setOverviewContent}>
+      <img className='poster' src={movie ? API_IMG+movie.profile_path  : unavailable} alt={movie.title || movie.name ? movie.title || movie.name : ""} />
+      <span className='rate'>Rated - {movie.vote_average ? movie.vote_average.toFixed(1) : ""}</span>
+    </div>
+    <div className='details'>
+      <h4 className='title'>{movie.title || movie.name ? movie.title || movie.name : ""}</h4>
+      <span className='date'>Released: {movie.moviefirst_air_date || movie.release_date || movie.known_for_department ? movie.first_air_date || movie.release_date || movie.known_for_department : ""}</span>
+    </div>
+  </div>
+  )
+}
+
+export default OverviewPeopleCard
