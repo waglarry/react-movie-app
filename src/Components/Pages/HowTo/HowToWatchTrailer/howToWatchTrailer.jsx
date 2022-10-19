e9331447645126be0fd2821f8a0decce14bc1a2c
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-19 10:54:46 +0000

    add content to howToWatch trailer component

--- src/Components/Pages/HowTo/HowToWatchTrailer/howToWatchTrailer.jsx
+++ src/Components/Pages/HowTo/HowToWatchTrailer/howToWatchTrailer.jsx
@@ -4,7 +4,30 @@ import Footer from '../../../Footer/Footer'
 const HowToWatchTrailer = () => {
   return (
     <>
-        <div>HowToWatchTrailer</div>
+        <div className='mainStepDiv'>
+            <h1 className='stepHeaderText'>How To Watch Trailer</h1>
+            <div className='stepsDiv'>
+                <p className='stepText'>Step 1: Click on the view button on a movie card to go to the overview page. or <br /> 
+                    Click on the Overview link in the footer to go to the overview page.
+                </p>
+                <div className='stepImageBox'>
+                    <img src="/HowToImages/downloadStep0.png" alt="Overview Link" />
+                </div>
+            </div>
+            <div className='stepsDiv'>
+                <p className='stepText'>Step 2: Click on the watch trailer button of your selected movie.</p>
+                <div className='stepImageBox'>
+                    <img src="/HowToImages/watchTrailerStep1.png" alt="Watch Trailer Button Image" />
+                </div>
+            </div>
+            <div className='stepsDiv'>
+                <p className='stepText'>Step 3: Success! Enjoy your video. <br /> 
+                 You can close the video tab at any time of your choice.</p>
+                <div className='stepImageBox'>
+                    <img src="/HowToImages/watchTrailerStep2.png" alt="Watch Trailer Video" />
+                </div>
+            </div>
+        </div>
         <Footer />
     </>
   )
