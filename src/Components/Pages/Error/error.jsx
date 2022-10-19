5da6d59a9c1f97fb9b347d8860c66121f3c07d21
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-19 14:29:55 +0000

    style ErrorPage

--- src/Components/Pages/Error/error.jsx
+++ src/Components/Pages/Error/error.jsx
@@ -1,16 +1,25 @@
+import { padding } from '@mui/system'
 import React from 'react'
 import Footer from '../../Footer/Footer'
+import { TbError404 } from 'react-icons/tb'
 
 const Error = () => {
   return (
     <>
       <div style={{
+      // position: "absolute",
+      // top: "50%",
+      // left: "50%",
+      // transform: "translate(-50%, -50%)",
+      fontSize: "1.5rem",
+      padding: "20rem 0"
+    }}><p style={{
       position: "absolute",
       top: "50%",
       left: "50%",
       transform: "translate(-50%, -50%)",
-      fontSize: "1.5rem"
-    }}>Page Not Found</div>
+      textAlign: "center",
+    }}><span style={{fontSize: "10rem"}}><TbError404 /></span><br /> Page Not Found</p></div>
     <Footer />
     </>
   )
