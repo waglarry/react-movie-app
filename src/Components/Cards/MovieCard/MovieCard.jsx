f0922280371e778852ef20f8d4bde70b10d07ddc
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-20 12:57:14 +0000

    remove button from movieCard

--- src/Components/Cards/MovieCard/MovieCard.jsx
+++ src/Components/Cards/MovieCard/MovieCard.jsx
@@ -6,15 +6,16 @@ import { Link } from 'react-router-dom'
 const MovieCard = ({title, released, poster, rate}) => {
   return (
     <div className='movieCard'>
+      <Link to={`/overview`} className="cardLinks">
       <div className='posterBox'>
         <img className='poster' src={ poster ? API_IMG+poster : unavailable} alt={title} />
-        <div className='overview'><Link to={`/overview`}><button className='posterViewBtn'>View</button></Link></div>
         <span className='rate'>Rated - {rate ? rate.toFixed(1) : ""}</span>
       </div>
       <div className='details'>
         <h4 className={title.length > 28 ? 'smallTitle' : 'title'}>{title}</h4>
         <span className='date'>Released: {released}</span>
       </div>
+      </Link>
     </div>
   )
 }
