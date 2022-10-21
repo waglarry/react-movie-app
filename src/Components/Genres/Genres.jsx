5510f0cc2b1e96d07d410917388679df7b80f467
acquaah-emmanuel <emmanuel.acquaah@amalitech.org>
2022-10-21 15:46:00 +0000

    edit genres

--- src/Components/Genres/Genres.jsx
+++ src/Components/Genres/Genres.jsx
@@ -1,35 +1,30 @@
-import React, { useEffect } from 'react'
-import Chip from '@mui/material/Chip';
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { API_KEY } from '../API/URL';
 import Box from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import { Select } from '@mui/material';
 
-const Genres = ({ genres,setGenres, selectedGenres, setSelectedGenres}) => {
+
+const Genres = ({ setSelectedGenres}) => {
+      const [genres, setGenres] = useState([])
 
      const fetchGenres = async () => {
         try {
             const { data } = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`);
             setGenres(data.genres)
         } catch (error) {
-            console.log(error)
+            console.log("")
         }
      }
 
      useEffect(() => {
        fetchGenres();
      }, [])
-     
-     const handleAddGenres = (genre) => {
-        setSelectedGenres([...selectedGenres, genre])
-        setGenres(genres && genres.filter(filtGenre => filtGenre && filtGenre.id !== genre.id))
-     }
-
-     const handleRemoveGenres = (genre) => {
-        setSelectedGenres(selectedGenres && selectedGenres.filter(selected => selected && selected.id !== genre.id));
-        setGenres([...genres, genre])
-     }
 
      const handleCheckboxChange = (event) => {
         const id = event.target.id;
@@ -51,7 +46,7 @@ const Genres = ({ genres,setGenres, selectedGenres, setSelectedGenres}) => {
         }
       }
 
-      const checkbox = genres.map((genre) => (
+      const checkbox = genres && genres.map((genre) => (
         <FormControlLabel
         key={genre.id}
         label={genre.name}
@@ -71,9 +66,49 @@ const Genres = ({ genres,setGenres, selectedGenres, setSelectedGenres}) => {
       />
       ))
 
+      const [genreId, setgenreId] = useState("")
+
+      const handleChange = (event) => {
+        const id = event.target.value;
+        setgenreId(id)
+        setSelectedGenres((selectedGenres) => 
+        selectedGenres.includes(id)
+            ? selectedGenres.filter(selected => selected.id !== id) 
+            : [...selectedGenres, id]
+        )
+        setSelectedGenres((selectedGenres) => 
+        selectedGenres.includes(id)
+            ? selectedGenres.filter(selected => selected.id !== id) 
+            : [...selectedGenres, id]
+        )
+      };
+
+      const genValues = genres && genres.map((genre) => (
+        <MenuItem value={genre.id}>{genre.name}</MenuItem>
+      ))
+
   return (
-    <div className='genresRow'>
+    <div>
+      <span className='ShowfilterText'>Filter By Genres</span>
+      <div className='genresRow'>
         {checkbox}
+    </div>
+    <div className='mobileGenres'>
+    <Box>
+      <FormControl fullWidth size="small" sx={{background: "#ccc", borderRadius: ".3rem"}}>
+        <InputLabel id="demo-simple-select-label">Genres</InputLabel>
+        <Select
+          labelId="demo-select-small"
+          id="demo-select-small"
+          value={genreId}
+          label="Genres"
+          onChange={handleChange}
+        >
+          {genValues}
+        </Select>
+      </FormControl>
+      </Box>
+    </div>
     </div>
   )
 }
