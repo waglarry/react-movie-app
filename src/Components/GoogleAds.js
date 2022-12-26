4293cd264fbbee14be4c5059ac472b659572376d
Emmanuel Acquaah <85497270+waglarry@users.noreply.github.com>
2022-12-26 08:09:23 +0000

    Create GoogleAds.js

---
+++ src/Components/GoogleAds.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class GoogleAds extends Component {
+
+    componentDidMount() {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+
+    render() {
+        return (
+                <ins className='adsbygoogle'
+                    style={{ display: 'block' }}
+                    data-ad-client= 'ca-pub-8350565651888273'
+                    data-ad-slot={this.props.slot}
+                    data-ad-format= 'auto'
+                    data-full-width-responsive="true"
+                >
+                </ins>
+        );
+    }
+}
+
+export default GoogleAds;
