05c828d1b27d80b5119b5425a63d9dcf8be68564
Emmanuel Acquaah <emmanuelacquaah2468@gmail.com>
2023-04-11 15:20:50 +0000

    remove unwanted comment from error component

--- src/Components/Pages/Error/error.jsx
+++ src/Components/Pages/Error/error.jsx
@@ -1,4 +1,3 @@
-import { padding } from '@mui/system'
 import React from 'react'
 import Footer from '../../Footer/Footer'
 import { TbError404 } from 'react-icons/tb'
@@ -7,10 +6,6 @@ const Error = () => {
   return (
     <>
       <div style={{
-      // position: "absolute",
-      // top: "50%",
-      // left: "50%",
-      // transform: "translate(-50%, -50%)",
       fontSize: "1.5rem",
       padding: "20rem 0"
     }}><p style={{
